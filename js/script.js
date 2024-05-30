35629d338f10e6d02f6c24a8b91df9b1533c13f6
Jahirul83 <jahirulislamcse83@gmail.com>
2024-05-30 14:33:36 +0600

    all querySelector and id taken

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 // script page
+let optionButton = document.querySelectorAll('.optionButtons');
+let advOptionButton = document.querySelectorAll('.advOptionButtons');
+let fontName = document.getElementById('fontName');
+let fontSize = document.getElementById('fontSize');
+
+let textInput = document.getElementById('text-input');
+
+let createLink = document.getElementById('createLink');
+
+let alignButtons = document.querySelectorAll('.align');
+
+let spacingButtons = document.querySelectorAll('.spacing');
+
+let formateButton = document.querySelectorAll(".formate");
+
+let scriptButton = document.querySelectorAll(".script");
+
+// fontLint
+let fontList = [
+    "Arial",
+    "Verdana",
+    "Times New Roman",
+    "Garamond",
+    "Georgi",
+    "Courser",
+    "cursive New",
+];
